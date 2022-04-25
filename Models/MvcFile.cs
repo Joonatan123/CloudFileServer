a4fe5495a46eb0a9d432e6ecd5fde552b8787785
Joonatan123 <joonatan137@gmail.com>
2022-04-25 13:54:39 +0300

    added code to display file sizes in a more readable format

--- Models/MvcFile.cs
+++ Models/MvcFile.cs
@@ -13,6 +13,7 @@ namespace CloudFileServer.Models
         //[DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public int Size { get; set; }
+        public string SizeString { get; set; }
         public string FullName;
         public MvcFile(string path, string rootDirectory)
         {
@@ -31,14 +32,37 @@ namespace CloudFileServer.Models
             string fileExtension = "";
             if (dotIndex != -1)
                 fileExtension = fileName.Substring(dotIndex);
-                
+
             Path = filePath;
             Date = File.GetCreationTime(file);
             Name = fileName;
             Extension = fileExtension;
             Size = (int)new System.IO.FileInfo(file).Length;
             FullName = Path + Name;
+            SizeString = "";
+            MakeSizeString();
+        }
+        public void MakeSizeString()
+        {
+            int unit = 0;
+            decimal tempSize = Size;
+            for (; tempSize >= 1000; unit++)
+            {
+                tempSize = tempSize / 1000;
+            }
+            int decimals = 0;
+            if (tempSize >= 100)
+                decimals = 0;
+            else if (tempSize >= 10)
+                decimals = 1;
+            else
+                decimals = 2;
+            tempSize = Math.Round(tempSize, decimals);
 
+            string[] unitString = new string[4]{"B","kB","MB","GB"};
+
+            SizeString = tempSize.ToString();
+            SizeString += " " + unitString[unit];
         }
     }
 }
